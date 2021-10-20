08db46b7048fb5bc9b6fc4f6f9d754191d8577cd
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-19 21:11:29 -0300

    Bloco de Código 5.7.5 da apostila 0x

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+// const dotenv = require('dotenv')
+// dotenv.config()
 const express = require ('express')
 const app = express()
 const mysql = require('mysql2')
@@ -5,24 +8,62 @@ const mysql = require('mysql2')
 app.use(express.json())
 
 
-const porta = process.env.PORT || 3000
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, PORT} = process.env
+const porta = PORT || 3000
+
 
 app.listen (porta, () => console.log (`Em execução. Porta: ${porta}.`))
 
 app.get('/medicos', (req, res) => {
     const connection  = mysql.createConnection({
-        host: 'localhost',
-        user: 'rodrigo',
-        password: '1234',
-        database: '20212_usjt_ccp1anmcc_hospital'
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE
     })
     const sql = 'SELECT * FROM tb_medico'
-    connection.query(
+    connection.execute(
         sql,
         (err, results, fields) => {
+            res.json(results)
+        }
+    )
+})
+
+app.post('/medicos', (req, res) => {
+    const connection = mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE
+    })
+    // const crm = req.body.crm
+    // const nome = req.body.nome
+    const { crm, nome } = req.body
+    // const sql = "INSERT INTO tb_medico (crm, nome) VALUES (" + crm + ", '" + nome + "')"
+    const sql = "INSERT INTO tb_medico (crm, nome) VALUES (?, ?)"
+    connection.execute(
+        sql,
+        [crm, nome],
+        (err, results, fields) => {
             console.log(results)
-            console.log(fields)
-            res.send('ok')
+            res.send('ok')            
+        }
+    )
+})
+
+app.get('/pacientes', (req, res) => {
+    const connection = mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE
+    })
+    const sql = 'SELECT * FROM tb_paciente'
+    connection.execute(
+        sql,
+        (err, results, fields) => {
+            res.json(results)
         }
     )
 })
